1210b5b9320384b24b086c6919baba34fb960559
t2-security-team <mac0016@naver.com>
2026-08-17 20:39:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -560,12 +560,12 @@ if not (p_all and g_all):
     
     with col1:
         st.markdown("##### 🔄 실시간 잡지")
-        st.markdown("<span style='font-size: 13px; color: #6b7280;'>현재 운영 중인 실시간 데이터 연동 버전입니다.  <첨두승객수 or 익일잡지></span>", unsafe_allow_html=True)
+        st.markdown("<span style='font-size: 13px; color: #6b7280;'>실시간 잡지.<br><첨두승객수 or 익일잡지></span>", unsafe_allow_html=True)
         st.link_button("이동하기", "https://live-magazine-t2.streamlit.app/", use_container_width=True)
         
     with col2:
         st.markdown("##### 💾 승객 수 파일저장")
-        st.markdown("<span style='font-size: 13px; color: #6b7280;'>날짜별 승객수 데이터를 미리 저장하고 관리하는 현재 버전입니다.</span>", unsafe_allow_html=True)
+        st.markdown("<span style='font-size: 13px; color: #6b7280;'>항공사 승객수 파일을 저장하고 관리.</span>", unsafe_allow_html=True)
         st.link_button("이동하기", "https://t2-pax-magazine.streamlit.app/", use_container_width=True)
         
     with col3:
